253a138a021394a240742aeb42613502a76961e7
Mike Jackson <perigran_falcon@yahoo.com>
2026-01-14 21:16:20 -0700

    Fixed comments.

--- Todo_List/config.php
+++ Todo_List/config.php
@@ -3,15 +3,15 @@
 # Database Configuration
 
 $servername = "localhost";
-$username = "mike"; // Replace with your MySQL username
-$password = "5454160s"; // Replace with your MySQL password
-$dbname = "php_web_apps"; // Replace with your database name
-$table_name = "todos"; // Replace with your table name
+$username = "foo";
+$password = "bar";
+$dbname = "php_web_apps";
+$table_name = "todos";
 
-// 1. Create connection
+# Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+# Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
